a62bb9951b2d7a3761ebe0e8222e517dd7d42841
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-09-25 20:57:49 +0200

    .

---
+++ Ejercicio1/ejercicio1.js
@@ -0,0 +1,14 @@
+function compararCadenas(cad1, cad2) {
+
+    if (typeof cad1 != 'string' || typeof cad2 != 'string') {
+        console.log('Error, una de las dos cadenas no es una cadena de caracteres')
+    } else {
+        if (cad1.length < cad2.length) {
+            console.log('La cadena '+cad1+' es la más corta')
+        } else {
+            console.log('La cadena '+cad2+' es la más corta')
+        }
+    }
+}
+
+compararCadenas('Hola me llamo miguel', 124)
---
+++ Ejercicio2/ejercicio2.js
@@ -0,0 +1,8 @@
+function compararCadenasIguales(cad1,cad2){
+    if (typeof cad1 != 'string' || typeof cad2 != 'string') {
+        console.log('Error, una de las dos cadenas no es una cadena de caracteres')
+    } else {
+        let cadenaAlReves
+    }
+}
+

87415aa74ab982c78ac7b803aada37d8b75c51f0
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-09-27 20:52:58 +0200

    .

--- Tema3/js/pruebas.js
+++ Tema3/js/pruebas.js
@@ -1,6 +1,6 @@
-document.documentElement // Devuelve el elemento raiz de document
-document.head // Devuelve el elemento <head>
-document.body // Devuelve el elemento <body>
-document.getElementById("id") // Devuelve el elemnto por su id
-document.getElementsByClassName("class") // Devuelve un objeto similar a un array de un elemnto por su nombre de clase
-document.getElementsByName("html tag") // Devuelve el un NodeList por el name del enlace
+console.log( document.documentElement) // Devuelve el elemento raiz de document
+console.log( document.head) // Devuelve el elemento <head>
+console.log( document.body) // Devuelve el elemento <body>
+console.log( document.getElementById("titulo1")) // Devuelve el elemnto por su id
+console.log( document.getElementsByClassName("parrafos")) // Devuelve un objeto similar a un array de un elemnto por su nombre de clase
+console.log( document.getElementsByName("a")) // Devuelve el un NodeList por el name del enlace

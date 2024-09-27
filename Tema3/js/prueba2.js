a6ac5711ff68949a637d92cd07dc2b0fa40a751a
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-09-27 20:10:00 +0200

    .

--- Tema3/js/prueba2.js
+++ Tema3/js/prueba2.js
@@ -1,3 +1,5 @@
 console.log("1. "+ new Date().toString());
 let id = setTimeout(() => console.log("2. "+ new Date().toString()),5000)
 console.log("3. "+ new Date().toString());
+
+confirm("¿Quieres pararla?")?clearTimeout(id):id;

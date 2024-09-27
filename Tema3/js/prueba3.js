b30f0d1ede93ff1fb271259d7f3dc3a4fff3c20f
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-09-27 20:46:00 +0200

    .

--- Tema3/js/prueba3.js
+++ Tema3/js/prueba3.js
@@ -1,7 +1,20 @@
-let num = 1;
-let idInterval = setInterval(function(){
-    console.log(num++);
-    if(num>10){
-        clearInterval(idInterval);
-    }
-}, 1000)
+// let num = 1;
+// let idInterval = setInterval(function(){
+//     console.log(num++);
+//     if(num>10){
+//         clearInterval(idInterval);
+//     }
+// }, 1000)
+
+console.log(location.href);
+console.log(location.host)
+console.log(location.port)
+console.log(location.protocol)
+//lacation.reload();
+location.assign("https://google.com")
+location.replace("https://google.com")
+
+console.log(history.length)
+history.back()
+history.forward();
+history.go(2)
---
+++ Tema3/js/pruebas.js
@@ -0,0 +1,6 @@
+document.documentElement // Devuelve el elemento raiz de document
+document.head // Devuelve el elemento <head>
+document.body // Devuelve el elemento <body>
+document.getElementById("id") // Devuelve el elemnto por su id
+document.getElementsByClassName("class") // Devuelve un objeto similar a un array de un elemnto por su nombre de clase
+document.getElementsByName("html tag") // Devuelve el un NodeList por el name del enlace

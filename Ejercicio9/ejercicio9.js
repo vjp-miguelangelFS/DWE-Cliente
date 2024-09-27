ef6d2546469427362117d398ae76b1b3a159b1f4
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-09-27 16:06:46 +0200

    .

--- Ejercicio9/ejercicio9.js
+++ Ejercicio9/ejercicio9.js
@@ -12,7 +12,7 @@ class platoCocina{
 let MapaPlatos = new Map();
 
 MapaPlatos.set(new platoCocina('Cocido',60,6),['Garbanzo','Sal','Patatas','Chorizo','Zanahorias']);
-MapaPlatos.set(new platoCocina('Tortilla',45,7),['Huevos,Sal','Patatas','Aceite','Cebolla']);
+MapaPlatos.set(new platoCocina('Tortilla',45,7),['Huevos','Sal','Patatas','Aceite','Cebolla']);
 MapaPlatos.set(new platoCocina('Berenjenas',150,7),['Berenjena','Carne','Aceite','Sal','Queso','Pimientos','Tomates','Bechamel']);
 
 for(let [persona,arrayIngredientes] of MapaPlatos){

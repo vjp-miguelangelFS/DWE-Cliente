81a9ec95119466bd65fd74bba135d542ff378a97
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-10-03 20:58:08 +0200

    .

---
+++ Tema3/Ejercicio3/js/ejercicio3.js
@@ -0,0 +1,14 @@
+// 1
+let selectorA = document.querySelector('a');
+
+// 2
+
+selectorA.setAttribute('id','aTwitter')
+
+// 3
+
+selectorA.setAttribute('href','https://twitter.x.com')
+
+// 4
+
+selectorA.setAttribute('')

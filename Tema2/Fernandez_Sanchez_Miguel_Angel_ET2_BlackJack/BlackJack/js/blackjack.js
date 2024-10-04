f85bef4bcbb4c80cd9fd4da78d67ba3cb2e4c0c9
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-10-04 17:00:52 +0200

    .

--- Tema2/Fernandez_Sanchez_Miguel_Angel_ET2_BlackJack/BlackJack/js/blackjack.js
+++ Tema2/Fernandez_Sanchez_Miguel_Angel_ET2_BlackJack/BlackJack/js/blackjack.js
@@ -1,12 +1,12 @@
-function crearMazo(){
-    let palos = ['Corazones','Picas','Treboles','Diamante'];
-    let valores = [1,2,3,4,5,6,7,8,9,10,'Jota','Reina','Rey'];
+function crearMazo() {
+    let palos = ['Corazones', 'Picas', 'Treboles', 'Diamante'];
+    let valores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jota', 'Reina', 'Rey'];
 
     let mazoCartas = [];
 
     for (let i = 0; i < palos.length; i++) {
         for (let f = 0; f < valores.length; f++) {
-            mazoCartas.push(palos[i],valores[f])
+            mazoCartas.push([palos[i], valores[f]])
         }
     }
     console.log(mazoCartas)
